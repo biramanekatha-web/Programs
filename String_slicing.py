60d95c7624bf08b94f52898d6d252e31fd140a44
kathabiramane <biramanekatha@gmail.com>
2026-05-20 14:50:24 +0530

    Updated programs

---
+++ String_slicing.py
@@ -0,0 +1,9 @@
+name="Katha Kailas Biramane"
+print(name[0:6:1])#Katha
+print(name[0:13:-1])#empty
+print(name[-5:4:2])#empty string 
+print(name[3:-2:-2])#emptystring
+print(name[1:-6:+2])#ah alsB
+print(name[-1:-7:+2])#empty string 
+print(name[-3:-11:-3])#ar 
+
